ea3838d807cd098e9090db4b8d5a8f8c75f28c19
Brendan Clark <iam.clarkbrendan@gmail.com>
2024-07-10 17:32:31 -0400

    got the pikmin spawning working, but as soon as you spawn 2 there is an error with flocking. Streamlined the player movement and it now uses custom input.

--- main.gd
+++ main.gd
@@ -1,11 +1,19 @@
 extends Node2D
-@export var pikmin: Pikmin
+@export var pikminScene: PackedScene
+
+var allPikmin: Array[Pikmin]
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pikmin.instantiate()
-	add_child(pikmin)
+	pass
+	
+func _input(event):
+	if event.is_action_pressed("create_pikmin"):
+		var pikmin = pikminScene.instantiate()
+		allPikmin.append(pikmin)
+		add_child(pikmin)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pikmin.set_target($Player.position)
+	for pikmin in allPikmin:
+		pikmin.set_target($Player.position)
